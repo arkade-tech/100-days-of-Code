1756af88b541b4b140f12a83db9ac9a5ecb08586
arkade-tech <arkade2006@gmail.com>
2026-09-06 12:30:29 +0530

    Day 26-Completed 2 C problems

---
+++ Day26/question52.c
@@ -0,0 +1,56 @@
+/*Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+*/
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+#include <stdio.h>
+int main() {
+
+    // 1 star
+    printf("*\n\n");
+
+    // 3 stars
+    for (int i = 1; i <= 3; i++)
+        printf("*\n");
+    printf("\n");
+
+    // 5 stars
+    for (int i = 1; i <= 5; i++)
+        printf("*\n");
+    printf("\n");
+
+    // 3 stars
+    for (int i = 1; i <= 3; i++)
+        printf("*\n");
+    printf("\n");
+
+    // 1 star
+    printf("*\n");
+
+    return 0;
+}
